17964a1db3b0f7d0877f1d6e54ec8816c1bfdff5
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-14 10:39:26 -0300

    build(tailwind): config colors and tipography

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      'light-purple': '#A328F5',
+      'white': '#F9F9F9',
+      'gray': '#828282',
+      'gray-200': '#333333',
+      'gray-400': '#212121',
+      'dark': '#171717',
+    },
+    extend: {
+      fontFamily: {
+        DM: ['DM Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
