7d084d6cebae125089d2544d8c6bf3393eba1593
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-14 12:25:35 -0300

    style(About): customize background color and position
    - Adjust backgroud color to gray-400
    - Postion with flex

---
+++ src/components/About/styles.ts
@@ -0,0 +1,15 @@
+import tw from 'tailwind-styled-components';
+
+export const Container = tw.article`
+  text-white
+  font-DM
+
+  bg-gray-400
+`;
+
+export const ContentContainer = tw.div`
+  container
+
+  flex
+  flex-col
+`;
