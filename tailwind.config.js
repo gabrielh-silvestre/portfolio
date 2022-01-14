cc8af222df06e3b0ceeefac1b2655fd97d28dd62
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-14 10:56:27 -0300

    build(tailwind): config container

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     colors: {
       'light-purple': '#A328F5',
       'white': '#F9F9F9',
