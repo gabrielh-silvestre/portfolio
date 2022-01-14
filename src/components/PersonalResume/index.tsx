8a92bc6a58b9df217648ca800f0510e5c1b3fcb6
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-14 12:41:37 -0300

    feat(components): create PersonalResume

---
+++ src/components/PersonalResume/index.tsx
@@ -0,0 +1,16 @@
+import { Container, Description, Title } from './styles';
+
+export function PersonalResume() {
+  return (
+    <Container>
+      <Title>About me</Title>
+
+      <Description>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error
+        voluptate quia necessitatibus pariatur iste, sit, voluptatum veritatis
+        repudiandae dolore ab hic in facere dolores nobis laborum voluptatibus
+        harum deserunt? Asperiores.
+      </Description>
+    </Container>
+  );
+}
