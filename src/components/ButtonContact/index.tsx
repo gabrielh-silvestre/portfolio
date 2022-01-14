955b07e3415c71b6ad09a7639e308be85400b85e
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2022-01-14 12:14:22 -0300

    feat(components): create ButtonContact
    - Create basic structure
    - Style using tailwind-style-components

---
+++ src/components/ButtonContact/index.tsx
@@ -0,0 +1,25 @@
+import tw from 'tailwind-styled-components';
+
+const Button = tw.button`
+  mr-2
+  px-7
+  py-3
+
+  text-white
+  font-DM
+  text-sm
+  font-medium
+
+  border
+  border-gray-200
+`;
+
+export function ButtonContact() {
+  return (
+    <Button
+      type="button"
+    >
+      Contact me
+    </Button>
+  );
+}
